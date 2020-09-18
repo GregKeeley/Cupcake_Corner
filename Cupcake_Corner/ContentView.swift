25778a81535ffebd82216fea3c3c93e1b792ca89
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-09-18 16:24:07 -0400

    Initial Commit

---
+++ Cupcake_Corner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cupcake_Corner
+//
+//  Created by Gregory Keeley on 9/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
