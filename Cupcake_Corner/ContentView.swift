1a86d99e5895cda6a4ecd7316c5951fbcbf2ed49
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-09-18 16:59:06 -0400

    - Building cupcake order form with swiftUI

--- Cupcake_Corner/ContentView.swift
+++ Cupcake_Corner/ContentView.swift
@@ -5,12 +5,50 @@
 //  Created by Gregory Keeley on 9/18/20.
 //
 
+import Combine
 import SwiftUI
 
+class Order: ObservableObject {
+    
+    var didChange = PassthroughSubject<Void, Never>()
+    static let types = ["Vanilla", "Choclate", "Strawberry", "Rainbow"] 
+    var type = 0 { didSet { update() } }
+    var quantity = 3 { didSet { update() } }
+    var extraFrosting = false { didSet { update() } }
+    var addSprinkles = false { didSet { update() } }
+    func update() {
+        didChange.send()
+    }
+    
+}
+
 struct ContentView: View {
+    @ObservedObject var order = Order()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker(selection: $order.type, label: Text("Select your cake type")) {
+                        ForEach(0 ..< Order.types.count) {
+                            Text(Order.types[$0]).tag($0)
+                        }
+                    }
+                    Stepper(value:  $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                Section {
+                    Toggle(isOn: $order.extraFrosting) {
+                        Text("Add extra frosting")
+                    }
+                    Toggle(isOn: $order.addSprinkles, label: {
+                        Text("Add extra sprinkles")
+                    })
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
